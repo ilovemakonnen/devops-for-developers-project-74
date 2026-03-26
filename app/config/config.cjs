2dd49494c60c4ddd5e61d5b5f47d3250dbbe07c4
ilovemakonnen <p_asmetkin@icloud.com>
2026-03-27 00:05:40 +0300

    done?

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -2,27 +2,29 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
+    dialect: 'postgres',
+    host: process.env.DATABASE_HOST || 'db',
+    port: 5432,
+    database: process.env.DATABASE_NAME || 'blog',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'password',
   },
-  production: {
-  dialect: 'postgres',
-  host: process.env.DB_HOST || 'db',                // обязательно имя сервиса
-  port: 5432,                // или process.env.DATABASE_PORT, если она = 5432
-  database: process.env.DATABASE_NAME || 'blog',
-  username: process.env.DATABASE_USERNAME || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'password',
-},
+
   test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
+    dialect: 'postgres',
+    host: process.env.DATABASE_HOST || 'db',
+    port: 5432,
+    database: process.env.DATABASE_NAME || 'blog',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'password',
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
+
+  production: {
+    dialect: 'postgres',
+    host: process.env.DATABASE_HOST || 'db',
+    port: 5432,
+    database: process.env.DATABASE_NAME || 'blog',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'password',
+  },
 };
